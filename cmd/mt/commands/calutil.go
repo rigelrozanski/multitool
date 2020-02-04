39e52c41995666f0dcd091655efe5dd1646c0702
rigelrozanski <rigel.rozanski@gmail.com>
2020-02-04 06:52:24 -0500

    daily-activities calutil

--- cmd/mt/commands/calutil.go
+++ cmd/mt/commands/calutil.go
@@ -3,12 +3,14 @@ package commands
 import (
 	"fmt"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/jung-kurt/gofpdf"
 	"github.com/spf13/cobra"
 
 	cmn "github.com/rigelrozanski/common"
+	"github.com/rigelrozanski/thranch/quac"
 )
 
 // speedy todolists
@@ -23,11 +25,113 @@ var (
 		Args:  cobra.ExactArgs(2),
 		RunE:  RipDaysCmd,
 	}
+	DailyActivities = &cobra.Command{
+		Use:   "dailyactivities [YYYY-MM-DD]",
+		Short: "print daily activities sheet beginning today (or at the specified date)",
+		Args:  cobra.MaximumNArgs(1),
+		RunE:  DailyActivitiesCmd,
+	}
 )
 
 func init() {
 	CalUtil.AddCommand(RipDays)
+	CalUtil.AddCommand(DailyActivities)
 	RootCmd.AddCommand(CalUtil)
+}
+
+func DailyActivitiesCmd(cmd *cobra.Command, args []string) error {
+
+	// get the date
+	startDate := time.Now()
+	if len(args) == 1 {
+		var err error
+		startDate, err = cmn.ParseYYYYdMMdDD(args[0])
+		if err != nil {
+			return err
+		}
+	}
+
+	pdf := gofpdf.New("Landscape", "in", "Letter", "")
+	pdf.SetMargins(0, 0, 0)
+	pdf.SetFont("courier", "", 12)
+	pdf.AddPage()
+
+	// read in activities
+	quac.Initialize(os.ExpandEnv("$HOME/.thranch_config"))
+	activitiesRaw := quac.GetForApp("daily-activities")
+	activities := strings.Split(activitiesRaw, "\n")
+
+	minX := float64(0.3)
+	maxX := float64(10.7)
+
+	xActivities := minX
+	yActivities := float64(1.5)
+	maxStrWidth := float64(0)
+	maxYActivity := float64(0)
+
+	for i, activity := range activities {
+		if len(activity) < 2 {
+			continue
+		}
+
+		// print text
+		y := yActivities + float64(i)*0.2
+		pdf.Text(xActivities, y, activity)
+
+		// update dimention saves
+		width := pdf.GetStringWidth(activity)
+		if maxStrWidth < width {
+			maxStrWidth = width
+		}
+		maxYActivity = y + 0.07
+	}
+
+	xDates := maxStrWidth + xActivities + 0.1
+	yDates := float64(1.3)
+	maxXDates := float64(0)
+
+	// print first vertical line
+	pdf.Line(xDates+0.07-0.2, 0.3, xDates+0.07-0.2, maxYActivity)
+
+	for i := 0; ; i++ {
+
+		// print date
+		pdf.TransformBegin()
+		pdf.TransformRotate(90, xDates, yDates)
+		date := startDate.Add(time.Duration(i) * 24 * time.Hour)
+		dateStr := date.Format(cmn.LayoutYYYYdMMdDD)
+		pdf.Text(xDates, yDates, dateStr)
+		pdf.TransformEnd()
+
+		// print vertical line
+		pdf.Line(xDates+0.07, 0.3, xDates+0.07, maxYActivity)
+
+		// break if past the max X
+		xDates = xDates + 0.2
+		if xDates > maxX {
+			break
+		}
+
+		maxXDates = xDates + 0.07
+	}
+
+	// print horizontal lines
+	pdf.Line(minX, yActivities-0.13, maxXDates, yActivities-0.13)
+	for i, activity := range activities {
+		if len(activity) < 2 {
+			continue
+		}
+		y := yActivities + float64(i)*0.2
+		pdf.Line(minX, y+0.07, maxXDates, y+0.07)
+
+	}
+
+	err := pdf.OutputFileAndClose("temp.pdf")
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func RipDaysCmd(cmd *cobra.Command, args []string) error {
--- go.mod
+++ go.mod
@@ -5,9 +5,11 @@ go 1.12
 require (
 	github.com/atotto/clipboard v0.1.2
 	github.com/jung-kurt/gofpdf v1.16.2
+	github.com/magiconair/properties v1.8.1
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/pdfcpu/pdfcpu v0.2.6-0.20191115001021-1e3294bc5ab2
 	github.com/rigelrozanski/common v0.0.0-20200204033706-d44f43da9cbb
+	github.com/rigelrozanski/thranch v0.0.0-20200204115042-6ec305409672
 	github.com/rigelrozanski/wb v0.0.0-20190430015828-95a7304b685b
 	github.com/spf13/cobra v0.0.5
 	github.com/spf13/pflag v1.0.5
@@ -16,3 +18,5 @@ require (
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
 	google.golang.org/api v0.15.0
 )
+
+//replace github.com/rigelrozanski/thranch => /Users/rigelrozanski/go/src/github.com/rigelrozanski/thranch
