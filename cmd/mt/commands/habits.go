db1bdd9296e30d8b5b3a581985b58bc9517a7846
rigelrozanski <rigel.rozanski@gmail.com>
2020-02-24 00:00:51 -0500

    stripe lines in habits

--- cmd/mt/commands/habits.go
+++ cmd/mt/commands/habits.go
@@ -1,7 +1,6 @@
 package commands
 
 import (
-	"fmt"
 	"os"
 	"strings"
 	"time"
@@ -49,7 +48,13 @@ func HabitsCmd(cmd *cobra.Command, args []string) error {
 	// read in activities
 	quac.Initialize(os.ExpandEnv("$HOME/.thranch_config"))
 	activitiesRaw := quac.GetForApp("habits")
-	activities := strings.Split(activitiesRaw, "\n")
+	activitiesUnfiltered := strings.Split(activitiesRaw, "\n")
+	var activities []string
+	for _, activity := range activitiesUnfiltered {
+		if len(activity) > 1 {
+			activities = append(activities, activity)
+		}
+	}
 
 	minX := margin
 	maxX := pageWidthInch - margin
@@ -57,34 +62,58 @@ func HabitsCmd(cmd *cobra.Command, args []string) error {
 	xActivities := minX
 	yActivities := float64(1.5)
 	maxStrWidth := float64(0)
+
 	maxYActivity := float64(0)
-
-	for i, activity := range activities {
-		if len(activity) < 2 {
-			continue
-		}
-
-		// print text
-		y := yActivities + float64(i)*0.2
-		pdf.Text(xActivities, y, activity)
-
-		// update dimention saves
-		width := pdf.GetStringWidth(activity)
-		if maxStrWidth < width {
-			maxStrWidth = width
-		}
-		maxYActivity = y + 0.07
-	}
+	maxXDates := float64(0)
 
 	xDates := maxStrWidth + xActivities + 0.1
 	yDates := float64(1.3)
-	maxXDates := float64(0)
 
-	// print first vertical line
-	pdf.Line(xDates+0.07-0.2, 0.3, xDates+0.07-0.2, maxYActivity)
-
+	// determine maxXDates, maxYActivity
+	stringWidthMargin := 0.1
+	for i, activity := range activities {
+		width := pdf.GetStringWidth(activity) + stringWidthMargin
+		if maxStrWidth < width {
+			maxStrWidth = width
+		}
+		y := yActivities + float64(i)*0.2
+		maxYActivity = y + 0.07
+	}
 	for i := 0; ; i++ {
+		// break if past the max X
+		xDates = xDates + 0.2
+		if xDates > maxX {
+			break
+		}
+		maxXDates = xDates + 0.07
+	}
 
+	// fill colour for the tiled rows
+	pdf.SetFillColor(230, 230, 230)
+
+	// print horizontal lines
+	pdf.Line(minX, yActivities-0.13, maxXDates, yActivities-0.13)
+	for i, _ := range activities {
+		y := yActivities + float64(i)*0.2
+		if i%2 == 0 && (i+1) < len(activities) {
+			y2 := yActivities + float64(i+1)*0.2
+			width := maxXDates - minX
+			height := y2 - y
+			pdf.Rect(minX, y+0.07, width, height, "F")
+		}
+		pdf.Line(minX, y+0.07, maxXDates, y+0.07)
+	}
+
+	// print horizontal text habit items
+	for i, activity := range activities {
+		y := yActivities + float64(i)*0.2
+		pdf.Text(xActivities, y, activity)
+	}
+
+	// print vertical lines and dates
+	xDates = maxStrWidth + xActivities + 0.1
+	pdf.Line(xDates+0.07-0.2, 0.3, xDates+0.07-0.2, maxYActivity) // print first vertical line
+	for i := 0; ; i++ {
 		// print date
 		pdf.TransformBegin()
 		pdf.TransformRotate(90, xDates, yDates)
@@ -92,43 +121,35 @@ func HabitsCmd(cmd *cobra.Command, args []string) error {
 		dateStr := date.Format(cmn.LayoutYYYYdMMdDD)
 		pdf.Text(xDates, yDates, dateStr)
 		pdf.TransformEnd()
-
-		// print vertical line
 		pdf.Line(xDates+0.07, 0.3, xDates+0.07, maxYActivity)
-
 		// break if past the max X
 		xDates = xDates + 0.2
 		if xDates > maxX {
 			break
 		}
-
-		maxXDates = xDates + 0.07
 	}
 
-	// print horizontal lines
-	pdf.Line(minX, yActivities-0.13, maxXDates, yActivities-0.13)
-	for i, activity := range activities {
-		if len(activity) < 2 {
-			continue
-		}
-		y := yActivities + float64(i)*0.2
-		pdf.Line(minX, y+0.07, maxXDates, y+0.07)
-
-	}
-
+	// ___________________ OUTPUT
 	err := pdf.OutputFileAndClose("temp.pdf")
 	if err != nil {
 		return err
 	}
 
-	// print the file
-	command1 := fmt.Sprintf("lp -d Brother_HL_L2320D_series temp.pdf")
-	output1, err := cmn.Execute(command1)
-	fmt.Printf("%v\n%v\n", command1, output1)
+	_, err = cmn.Execute("open temp.pdf")
 	if err != nil {
 		return err
 	}
 
-	// remove the temp file
-	return os.Remove("temp.pdf")
+	return nil
+
+	// TODO integrate with flag
+	//// print the file
+	//command1 := fmt.Sprintf("lp -d Brother_HL_L2320D_series temp.pdf")
+	//output1, err := cmn.Execute(command1)
+	//fmt.Printf("%v\n%v\n", command1, output1)
+	//if err != nil {
+	//return err
+	//}
+	//// remove the temp file
+	//return os.Remove("temp.pdf")
 }
